55350a22c305943188201b4ebf916c893e41adf3
Blanche Haddad <blancherachel@gmail.com>
2023-04-11 14:18:30 -0600

    fix: Routes and imports

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,6 +1,9 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import { fetchArticles } from '../../apiCalls';
+import { List } from '../List/List';
+import { Details } from '../Details/Details';
 
 function App() {
   const [articles, setArticles] = useState([]);
@@ -8,14 +11,25 @@ function App() {
   const [error, setError] = useState("");
 
   useEffect(() => {
+    setLoading(true)
     fetchArticles()
-      .then(data => console.log(data.results))
-      .catch(err => console.log(err))
+      .then(data => {
+        setLoading(false);
+        console.log(data.results)
+        setArticles(data.results)
+      })
+      .catch(err => {
+        console.log(err)
+        setError("Failed to fetch articles.")
+      })
   }, [])
 
   return (
     <div>
-
+      <Routes>
+        <Route path="/" element={<List articles={articles} />} />
+        <Route path="/details/:id" element={<Details />} />
+      </Routes>
     </div>
   )
 }
