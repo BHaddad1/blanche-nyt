48355d49a8338679dd49cb0460ceafb0143036ba
Blanche Haddad <blancherachel@gmail.com>
2023-04-13 10:54:14 -0600

    feat: Update styling

--- src/Components/Details/Details.jsx
+++ src/Components/Details/Details.jsx
@@ -1,4 +1,6 @@
 import { useLocation } from "react-router";
+import { NavLink } from "react-router-dom";
+import "./Details.css";
 
 export const Details = () => {
 
@@ -7,14 +9,15 @@ export const Details = () => {
   console.log(state);
 
   return (
-    <div>
+    <div className="details-container">
+      <NavLink to="/" className="nav-link-details">Back Home</NavLink>
       <h2 className="details-header">Article Details</h2>
       <img src={state.media[0]["media-metadata"][0].url} alt="Article Photo" className="details-img"/>
       <h3 className="details-title">{state.title}</h3>
       <p className="p-details">{state.byline}</p>
+      <a href={state.url} className="p-details">{state.url}</a>
       <p className="p-details">Abstract: {state.abstract}</p>
       <p className="p-details">Published: {state.published_date}</p>
-      <p className="p-details">Article Link: {state.url}</p>
     </div>
   )
 }
